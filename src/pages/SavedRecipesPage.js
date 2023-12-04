bbf36f673c3e25fb8a5192d7f6818c0ee2966941
Sherriff Kadiri <sherriff.kadiri@ucalgary.ca>
2023-12-03 19:00:56 -0700

    Saved recipes

--- src/pages/SavedRecipesPage.js
+++ src/pages/SavedRecipesPage.js
@@ -1,29 +1,240 @@
-import {Text, TextInput, View, StyleSheet, TouchableHighlight, ScrollView} from "react-native";
+import { Text, View, StyleSheet, ScrollView, TouchableOpacity, Image } from "react-native";
 
-import React from "react";
+import { React, useState } from "react";
 
-export default function SavedRecipesPage () {
-    const [text, onChangeText] = React.useState('');
+import Icon from 'react-native-vector-icons/Ionicons';
+import StarRating from 'react-native-star-rating-widget';
+
+export default function SavedRecipesPage ({ navigation }) {
+    const [rating, setRating] = useState(0);
+    const [heartPressed, setHeartPressed] = useState(false);
 
     return (
         <ScrollView>
             <View style={styles.container}>
                 <View style={{width: '100%'}}>
                     <ScrollView style={cardStyles.container}>
-                        <View style={[cardStyles.card, cardStyles.cardElevated]}>
-                            <Text>Press Here</Text>
+                        <View style={cardStyles.fullCard}>
+                            <View style={[cardStyles.card, cardStyles.cardElevated]}>
+                                <View>
+                                    <TouchableOpacity onPress={() => navigation.navigate('TestRecipe')} style={[cardStyles.card, cardStyles.cardElevated]}>
+                                        <View style={cardStyles.backgroundImage}>
+                                            <Image
+                                                source={require('../res/bread2.png')}
+                                                style={{resizeMode: 'cover', width: 105, height: 100}}
+                                            />
+                                        </View>
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                            <View style={[cardStyles.cardInfo, cardStyles.cardElevated]}>
+                                <Text style={cardStyles.itemText}>English White Bread </Text>
+                                <StarRating
+                                    rating={rating}
+                                    onChange={setRating}
+                                />
+                                <TouchableOpacity underlayColor={'#ff85fb'} onPress={() => {setHeartPressed(heartPressed => !heartPressed)}}>
+                                    <View>
+                                        {heartPressed === true ? <Icon name="heart" size={30} color="#ff85fb" /> : <Icon name="heart-outline" size={30} color="#ff85fb"/>}
+                                    </View>
+                                </TouchableOpacity>
+                            </View>
                         </View>
-                        <View style={[cardStyles.card, cardStyles.cardElevated]}>
-                            <Text>Press Here</Text>
+                        <View style={cardStyles.fullCard}>
+                            <View style={[cardStyles.card, cardStyles.cardElevated]}>
+                                <View>
+                                    <TouchableOpacity onPress={() => navigation.navigate('TestRecipe2')} style={[cardStyles.card, cardStyles.cardElevated]}>
+                                        <View style={cardStyles.backgroundImage}>
+                                            <Image
+                                                source={require('../res/bread1.jpg')}
+                                                style={{resizeMode: 'cover', width: 105, height: 100}}
+                                            />
+                                        </View>
+                                    </TouchableOpacity>
+                                    
+                                </View>
+                                
+                            </View>
+                            <View style={[cardStyles.cardInfo, cardStyles.cardElevated]}>
+                                <Text style={cardStyles.itemText}>English White Bread </Text>
+                                <StarRating
+                                    rating={rating}
+                                    onChange={setRating}
+                                />
+                                <TouchableOpacity underlayColor={'#ff85fb'} onPress={() => {setHeartPressed(heartPressed => !heartPressed)}}>
+                                    <View>
+                                        {heartPressed === true ? <Icon name="heart" size={30} color="#ff85fb" /> : <Icon name="heart-outline" size={30} color="#ff85fb"/>}
+                                    </View>
+                                </TouchableOpacity>
+                            </View>
                         </View>
-                        <View style={[cardStyles.card, cardStyles.cardElevated]}>
-                            <Text>Press Here</Text>
+                         <View style={cardStyles.fullCard}>
+                            <View style={[cardStyles.card, cardStyles.cardElevated]}>
+                                <View>
+                                    <TouchableOpacity onPress={() => navigation.navigate('TestRecipe3')} style={[cardStyles.card, cardStyles.cardElevated]}>
+                                        <View style={cardStyles.backgroundImage}>
+                                            <Image
+                                                source={require('../res/bread3.jpeg')}
+                                                style={{resizeMode: 'cover', width: 105, height: 100}}
+                                            />
+                                        </View>
+                                    </TouchableOpacity>
+                                    
+                                </View>
+                                
+                            </View>
+                            <View style={[cardStyles.cardInfo, cardStyles.cardElevated]}>
+                                <Text style={cardStyles.itemText}>English White Bread </Text>
+                                <StarRating
+                                    rating={rating}
+                                    onChange={setRating}
+                                />
+                                <TouchableOpacity underlayColor={'#ff85fb'} onPress={() => {setHeartPressed(heartPressed => !heartPressed)}}>
+                                    <View>
+                                        {heartPressed === true ? <Icon name="heart" size={30} color="#ff85fb" /> : <Icon name="heart-outline" size={30} color="#ff85fb"/>}
+                                    </View>
+                                </TouchableOpacity>
+                            </View>
                         </View>
-                        <View style={[cardStyles.card, cardStyles.cardElevated]}>
-                            <Text>Press Here</Text>
+                         <View style={cardStyles.fullCard}>
+                            <View style={[cardStyles.card, cardStyles.cardElevated]}>
+                                <View>
+                                    <TouchableOpacity onPress={() => navigation.navigate('TestRecipe')} style={[cardStyles.card, cardStyles.cardElevated]}>
+                                        <View style={cardStyles.backgroundImage}>
+                                            <Image
+                                                source={require('../res/bread2.png')}
+                                                style={{resizeMode: 'cover', width: 105, height: 100}}
+                                            />
+                                        </View>
+                                    </TouchableOpacity>
+                                    
+                                </View>
+                                
+                            </View>
+                            <View style={[cardStyles.cardInfo, cardStyles.cardElevated]}>
+                                <Text style={cardStyles.itemText}>English White Bread </Text>
+                                <StarRating
+                                    rating={rating}
+                                    onChange={setRating}
+                                />
+                                <TouchableOpacity underlayColor={'#ff85fb'} onPress={() => {setHeartPressed(heartPressed => !heartPressed)}}>
+                                    <View>
+                                        {heartPressed === true ? <Icon name="heart" size={30} color="#ff85fb" /> : <Icon name="heart-outline" size={30} color="#ff85fb"/>}
+                                    </View>
+                                </TouchableOpacity>
+                            </View>
                         </View>
-                        <View style={[cardStyles.card, cardStyles.cardElevated]}>
-                            <Text>Press Here</Text>
+                         <View style={cardStyles.fullCard}>
+                            <View style={[cardStyles.card, cardStyles.cardElevated]}>
+                                <View>
+                                    <TouchableOpacity onPress={() => navigation.navigate('TestRecipe')} style={[cardStyles.card, cardStyles.cardElevated]}>
+                                        <View style={cardStyles.backgroundImage}>
+                                            <Image
+                                                source={require('../res/bread2.png')}
+                                                style={{resizeMode: 'cover', width: 105, height: 100}}
+                                            />
+                                        </View>
+                                    </TouchableOpacity>
+                                    
+                                </View>
+                                
+                            </View>
+                            <View style={[cardStyles.cardInfo, cardStyles.cardElevated]}>
+                                <Text style={cardStyles.itemText}>English White Bread </Text>
+                                <StarRating
+                                    rating={rating}
+                                    onChange={setRating}
+                                />
+                                <TouchableOpacity underlayColor={'#ff85fb'} onPress={() => {setHeartPressed(heartPressed => !heartPressed)}}>
+                                    <View>
+                                        {heartPressed === true ? <Icon name="heart" size={30} color="#ff85fb" /> : <Icon name="heart-outline" size={30} color="#ff85fb"/>}
+                                    </View>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                         <View style={cardStyles.fullCard}>
+                            <View style={[cardStyles.card, cardStyles.cardElevated]}>
+                                <View>
+                                    <TouchableOpacity onPress={() => navigation.navigate('TestRecipe')} style={[cardStyles.card, cardStyles.cardElevated]}>
+                                        <View style={cardStyles.backgroundImage}>
+                                            <Image
+                                                source={require('../res/bread2.png')}
+                                                style={{resizeMode: 'cover', width: 105, height: 100}}
+                                            />
+                                        </View>
+                                    </TouchableOpacity>
+                                    
+                                </View>
+                                
+                            </View>
+                            <View style={[cardStyles.cardInfo, cardStyles.cardElevated]}>
+                                <Text style={cardStyles.itemText}>English White Bread </Text>
+                                <StarRating
+                                    rating={rating}
+                                    onChange={setRating}
+                                />
+                                <TouchableOpacity underlayColor={'#ff85fb'} onPress={() => {setHeartPressed(heartPressed => !heartPressed)}}>
+                                    <View>
+                                        {heartPressed === true ? <Icon name="heart" size={30} color="#ff85fb" /> : <Icon name="heart-outline" size={30} color="#ff85fb"/>}
+                                    </View>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                         <View style={cardStyles.fullCard}>
+                            <View style={[cardStyles.card, cardStyles.cardElevated]}>
+                                <View>
+                                    <TouchableOpacity onPress={() => navigation.navigate('TestRecipe')} style={[cardStyles.card, cardStyles.cardElevated]}>
+                                        <View style={cardStyles.backgroundImage}>
+                                            <Image
+                                                source={require('../res/bread2.png')}
+                                                style={{resizeMode: 'cover', width: 105, height: 100}}
+                                            />
+                                        </View>
+                                    </TouchableOpacity>
+                                    
+                                </View>
+                                
+                            </View>
+                            <View style={[cardStyles.cardInfo, cardStyles.cardElevated]}>
+                                <Text style={cardStyles.itemText}>English White Bread </Text>
+                                <StarRating
+                                    rating={rating}
+                                    onChange={setRating}
+                                />
+                                <TouchableOpacity underlayColor={'#ff85fb'} onPress={() => {setHeartPressed(heartPressed => !heartPressed)}}>
+                                    <View>
+                                        {heartPressed === true ? <Icon name="heart" size={30} color="#ff85fb" /> : <Icon name="heart-outline" size={30} color="#ff85fb"/>}
+                                    </View>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                         <View style={cardStyles.fullCard}>
+                            <View style={[cardStyles.card, cardStyles.cardElevated]}>
+                                <View>
+                                    <TouchableOpacity onPress={() => navigation.navigate('TestRecipe')} style={[cardStyles.card, cardStyles.cardElevated]}>
+                                        <View style={cardStyles.backgroundImage}>
+                                            <Image
+                                                source={require('../res/bread2.png')}
+                                                style={{resizeMode: 'cover', width: 105, height: 100}}
+                                            />
+                                        </View>
+                                    </TouchableOpacity>
+                                    
+                                </View>
+                                
+                            </View>
+                            <View style={[cardStyles.cardInfo, cardStyles.cardElevated]}>
+                                <Text style={cardStyles.itemText}>English White Bread </Text>
+                                <StarRating
+                                    rating={rating}
+                                    onChange={setRating}
+                                />
+                                <TouchableOpacity underlayColor={'#ff85fb'} onPress={() => {setHeartPressed(heartPressed => !heartPressed)}}>
+                                    <View>
+                                        {heartPressed === true ? <Icon name="heart" size={30} color="#ff85fb" /> : <Icon name="heart-outline" size={30} color="#ff85fb"/>}
+                                    </View>
+                                </TouchableOpacity>
+                            </View>
                         </View>
                     </ScrollView>
                 </View>
@@ -42,16 +253,38 @@ const cardStyles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        width: 400,
-        height: 400,
-        margin: 6
+        height: 100,
+        margin: 6,
+        flexBasis: 'auto',
+    },
+
+    cardInfo: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 6,
+        backgroundColor: 'white',
+        flexBasis: 180,
+        flexGrow: 1
+    },
+
+    fullCard: {
+        flex: 1,
+        flexDirection: 'row',
+        height: 112
+        
+        
     },
 
     cardElevated: {
         backgroundColor: 'green',
     },
 
-
+    itemText: {
+        fontSize: 20,
+        fontFamily: 'serif',
+        alignSelf: 'center',
+    },
 })
 
 const styles = StyleSheet.create({
