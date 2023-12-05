bb547fb4591c20bbef46f9e302ac34582f97cded
Sherriff Kadiri <sherriff.kadiri@ucalgary.ca>
2023-12-05 00:40:12 -0700

    merged changes + updated saved recipes

--- src/pages/SavedRecipesPage.js
+++ src/pages/SavedRecipesPage.js
@@ -4,9 +4,10 @@ import { React, useState } from "react";
 
 import Icon from 'react-native-vector-icons/Ionicons';
 import StarRating from 'react-native-star-rating-widget';
+import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default function SavedRecipesPage ({ navigation }) {
-    const [rating] = useState([4.6, 3.9, 5]);
+    const [rating] = useState([4.6, 3.9, 5.0]);
     const [heartPressed, setHeartPressed] = useState(new Array(8).fill(true));
 
     const updateHeartPressed = (index) => {
@@ -15,7 +16,7 @@ export default function SavedRecipesPage ({ navigation }) {
         setHeartPressed(updatedHearts);
     };
 
-    const renderRecipeItem = (index, imageSource, recipeName) => (
+    const renderRecipeItem = (index, imageSource, title, rating, time, difficulty) => (
         <TouchableOpacity
           key={index}
           onPress={() => navigation.navigate(`TestRecipe${index + 1}`)}
@@ -33,7 +34,7 @@ export default function SavedRecipesPage ({ navigation }) {
           />
           <View style={cardStyles.cardInfo}>
             <View style={cardStyles.nameAndHeart}>
-              <Text style={cardStyles.itemText}>{recipeName}</Text>
+              <Text style={cardStyles.itemText}>{title}</Text>
               <TouchableOpacity
                 underlayColor={'#ff85fb'}
                 style={{}}
@@ -50,14 +51,20 @@ export default function SavedRecipesPage ({ navigation }) {
                 </View>
               </TouchableOpacity>
             </View>
-            <View style={cardStyles.starsAndRating}>
-              <StarRating
-                rating={rating[index]}
-                starSize={20}
-                starStyle={{ marginLeft: 0, marginRight: 0 }}
-              />
-              <Text style={{ color: 'grey', fontSize: 12, alignSelf: 'center', marginLeft: 5 }}>{rating[index]}</Text>
+            <View style={cardStyles.iconButtonRow}>
+            <View style={[cardStyles.iconContainer, { width: '25%' }]} >
+              <Icon name="star" size={25} color="#FFEA00" />
+              <Text style={cardStyles.customButtonText}>{rating}</Text>
             </View>
+            <View style={[cardStyles.iconContainer, { width: '35%' }]} >
+              <Icon name="timer-outline" size={25} color="black" style={{ top: 1.5 }} />
+              <Text style={cardStyles.customButtonText}>{time}</Text>
+            </View>
+            <View style={[cardStyles.iconContainer, { width: '30%' }]} >
+              <MaterialIcon name="gauge-low" size={25} color="green" />
+              <Text style={cardStyles.customButtonText}>{difficulty}</Text>
+            </View>
+          </View>
           </View>
         </TouchableOpacity>
       );
@@ -68,10 +75,10 @@ export default function SavedRecipesPage ({ navigation }) {
           <View style={styles.container}>
             <View style={{ width: '100%' }}>
               <ScrollView>
-                {renderRecipeItem(0, require('../res/bread2.png'), 'English White Bread')}
-                {renderRecipeItem(1, require('../res/bread1.jpg'), 'Canadian White Bread')}
-                {renderRecipeItem(2, require('../res/bread3.jpeg'), 'Italian Garlic Bread')}
-                {/* {renderRecipeItem(3, require('../res/bread2.png'), 'English White Bread')}
+                {renderRecipeItem(0, require('../res/bread2.png'), 'English White Bread', rating[0], 30, 'Easy')}
+                {renderRecipeItem(1, require('../res/bread1.jpg'), 'Canadian White Bread', rating[1], 30, 'Easy')}
+                {renderRecipeItem(2, require('../res/bread3.jpeg'), 'Italian Garlic Bread', rating[2], 30, 'Easy')}
+                {/* {renderRecipeItem(3, require('../res/bread2.png'), 'English White Bread', 4.6, 30, 'Easy')}
                 {renderRecipeItem(4, require('../res/bread2.png'), 'English White Bread')}
                 {renderRecipeItem(5, require('../res/bread2.png'), 'English White Bread')}
                 {renderRecipeItem(6, require('../res/bread2.png'), 'English White Bread')}
@@ -141,6 +148,34 @@ const cardStyles = StyleSheet.create({
         fontSize: 17,
         fontWeight: 'bold',
         fontFamily: 'serif',
+    },
+
+    iconContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        width: 10,
+        padding: 7,
+        marginVertical: 5,
+        alignItems: 'center',
+
+        borderRadius: 15,
+    },
+
+    customButtonText: {
+        fontWeight: 'bold',
+        color: 'black',
+        fontSize: 14,
+        marginLeft: 10,
+        fontFamily: 'serif',
+    },
+
+    iconButtonRow: {
+        marginTop: -5,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-evenly',
+        width: '100%',
+        alignSelf: 'center'
     },
 })
 
