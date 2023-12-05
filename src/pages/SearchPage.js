fde6eb0031b07042ff5b4acb4d45bad6f94e9dc4
Sherriff Kadiri <sherriff.kadiri@ucalgary.ca>
2023-12-05 02:49:11 -0700

    search works for 'bread' or 'Bread'

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -4,7 +4,8 @@ import { React, useState } from "react";
 import CustomModal from "../components/CustomModals";
 
 export default function SearchPage ({ navigation }) {
-    const [text, onChangeText] = useState('');
+    const [text, setText] = useState('');
+    const [searched, setSearched] = useState(false);
     const [popularModalVisible, setPopularModalVisible] = useState(false);
     //const [timeModalVisible, setTimeModalVisible] = useState(false);
     //const [ingredientsModalVisible, setIngredientModalVisible] = useState(false);
@@ -14,24 +15,42 @@ export default function SearchPage ({ navigation }) {
     const [starOneRating, setStarOneRating] = useState(0);
     const [starTwoRating, setStarTwoRating] = useState(0);
 
+    const RecipeCard = ({ navigation, navigateTo, imageSource, title }) => {
+        return (
+            <View>
+                <TouchableOpacity onPress={() => navigation.navigate(navigateTo)} style={[cardStyles.card, cardStyles.cardElevated]}>
+                    <View style={cardStyles.backgroundImage}>
+                        <Image
+                            source={imageSource}
+                            style={{resizeMode: 'cover', width: 187, height: 200}}
+                        />
+                    </View>
+                </TouchableOpacity>
+                <Text style={cardStyles.itemText}>{title}</Text>
+            </View>
+        );
+    };
+
+    
+
     return (
             <View style={styles.container}>
                 <Text style={{alignSelf: 'center',
-        fontSize: 25,
-        fontWeight: 'bold',
-        fontFamily: 'serif',
-        alignSelf: 'center'}}>
+                    fontSize: 25,
+                    fontWeight: 'bold',
+                    fontFamily: 'serif',
+                    alignSelf: 'center'}}>
                     What are you Cooking Today?
                 </Text>
                 <View style={styles.row}>
                     <TextInput
                         style={styles.inputContainer}
                         placeholder="Search"
-                        onChangeText={onChangeText}
+                        onChangeText={(text) => {setText(text); setSearched(false)}}
                         value={text}
                     />
-                    <TouchableOpacity underlayColor={'#3b50f3'} style={styles.customButtonContainer} >
-                        <Text style={styles.customButtonText}>Search</Text>
+                    <TouchableOpacity underlayColor={'#3b50f3'} style={styles.customButtonContainer} onPress={() => setSearched(true)}>
+                        <Text style={[styles.customButtonText, {color: 'white'}]}>Search</Text>
                     </TouchableOpacity>
                 </View>
                 <CustomModal
@@ -65,83 +84,52 @@ export default function SearchPage ({ navigation }) {
                         <Text style={styles.customButtonText}>Ingredients</Text>
                     </TouchableOpacity>
                     <TouchableOpacity 
-                    onPress={() => setRatingOneModalVisible(true)}
-                    underlayColor={'#3b50f3'} style={styles.rowButtonContainer} >
+                        onPress={() => setRatingOneModalVisible(true)}
+                        underlayColor={'#3b50f3'} style={styles.rowButtonContainer} >
                         <Text style={styles.customButtonText}>Ratings</Text>
                     </TouchableOpacity>
                 </View>
                 
                 <View style={{width: '100%', paddingHorizontal: 8}}>
-                    <Text style={cardStyles.headingText}>Recent Searches</Text>
-                    <ScrollView showsVerticalScrollIndicator={false} style={cardStyles.scrollContainer}>
-                        <View style={cardStyles.container}>
-                            <View>
-                                <TouchableOpacity onPress={() => navigation.navigate('TestRecipe')} style={[cardStyles.card, cardStyles.cardElevated]}>
-                                    <View style={cardStyles.backgroundImage}>
-                                        <Image
-                                            source={require('../res/bread2.png')}
-                                            style={{resizeMode: 'cover', width: 187, height: 200}}
-                                        />
+                    {(text === "bread" || text === "Bread") && searched === true ?
+                            <>
+                                <Text style={cardStyles.headingText}>Results</Text>
+                                <ScrollView showsVerticalScrollIndicator={false} style={cardStyles.scrollContainer}>
+                                    <View style={cardStyles.container}>
+                                        <RecipeCard navigation={navigation} navigateTo={'TestRecipe'} imageSource={require('../res/bread2.png')} title="English White Bread"/>
+                                        <RecipeCard navigation={navigation} navigateTo={'TestRecipe2'} imageSource={require('../res/bread1.jpg')} title="Canadian White Bread"/>
+                                        <RecipeCard navigation={navigation} navigateTo={'TestRecipe3'} imageSource={require('../res/bread3.jpeg')} title="Italian Garlic Bread"/>
                                     </View>
-                                </TouchableOpacity>
-                                <Text style={cardStyles.itemText}>English White Bread </Text>
-                            </View>
-                            <View>
-                                <TouchableOpacity onPress={() => navigation.navigate('TestRecipe2')} style={[cardStyles.card, cardStyles.cardElevated]}>
-                                    <View style={cardStyles.backgroundImage}>
-                                        <Image
-                                            source={require('../res/bread1.jpg')}
-                                            style={{resizeMode: 'cover', width: 187, height: 200}}
-                                        />
-                                    </View>
-                                </TouchableOpacity>
-                                <Text style={cardStyles.itemText}>Canadian White Bread</Text>
-                            </View>
-                            <View>
-                                <TouchableOpacity onPress={() => navigation.navigate('TestRecipe3')} style={[cardStyles.card, cardStyles.cardElevated]}>
-                                    <View style={cardStyles.backgroundImage}>
-                                        <Image
-                                            source={require('../res/bread3.jpeg')}
-                                            style={{resizeMode: 'cover', width: 187, height: 200}}
-                                        />
-                                    </View>
-                                </TouchableOpacity>
-                                <Text style={cardStyles.itemText}>Italian Garlic Bread</Text>
-                            </View>
-                            <View>
-                                <TouchableOpacity onPress={() => navigation.navigate('TestRecipe')} style={[cardStyles.card, cardStyles.cardElevated]}>
-                                    <View style={cardStyles.backgroundImage}>
-                                        <Image
-                                            source={require('../res/bread2.png')}
-                                            style={{resizeMode: 'cover', width: 187, height: 200}}
-                                        />
-                                    </View>
-                                </TouchableOpacity>
-                                <Text style={cardStyles.itemText}>English White Bread </Text>
-                            </View>
-                            <View style={[cardStyles.card, cardStyles.cardElevated]}>
-                                <Text>Press Here</Text>
-                            </View>
-                            <View style={[cardStyles.card, cardStyles.cardElevated]}>
-                                <Text>Press Here</Text>
-                            </View>
-                            <View style={[cardStyles.card, cardStyles.cardElevated]}>
-                                <Text>Press Here</Text>
-                            </View>
-                            <View style={[cardStyles.card, cardStyles.cardElevated]}>
-                                <Text>Press Here</Text>
-                            </View>
-                            <View style={[cardStyles.card, cardStyles.cardElevated]}>
-                                <Text>Press Here</Text>
-                            </View>
-                            <View style={[cardStyles.card, cardStyles.cardElevated]}>
-                                <Text>Press Here</Text>
-                            </View>
-                            <View style={[cardStyles.card, cardStyles.cardElevated]}>
-                                <Text>Press Here</Text>
-                            </View>
-                        </View>
-                    </ScrollView>
+                                </ScrollView>
+                            </>
+                    : 
+                        <> 
+                            {text != "bread" && text != "Bread" && text != '' && searched === true ?
+                                <>
+                                    <Text style={cardStyles.headingText}>Results</Text>
+                                    <ScrollView showsVerticalScrollIndicator={false} style={cardStyles.scrollContainer}>
+                                        <View style={cardStyles.container}>
+                                            <Text style={[cardStyles.headingText, {alignSelf: 'center'}]}>No Results</Text>
+                                        </View>
+                                    </ScrollView>
+                                </>
+                            :
+                                <> 
+                                    <Text style={cardStyles.headingText}>Recent Searches</Text>
+                                    <ScrollView showsVerticalScrollIndicator={false} style={cardStyles.scrollContainer}>
+                                        <View style={cardStyles.container}>
+                                            <RecipeCard navigation={navigation} navigateTo={'TestRecipe'} imageSource={require('../res/cupcake1.jpeg')} title="Gala Cupcake"/>
+                                            <RecipeCard navigation={navigation} navigateTo={'TestRecipe'} imageSource={require('../res/cookie1.jpeg')} title="Chuncky Cookies"/>
+                                            <RecipeCard navigation={navigation} navigateTo={'TestRecipe'} imageSource={require('../res/bread2.png')} title="English White Bread"/>
+                                            <RecipeCard navigation={navigation} navigateTo={'TestRecipe2'} imageSource={require('../res/bread1.jpg')} title="Canadian White Bread"/>
+                                            <RecipeCard navigation={navigation} navigateTo={'TestRecipe3'} imageSource={require('../res/bread3.jpeg')} title="Italian Garlic Bread"/>
+                                            <RecipeCard navigation={navigation} navigateTo={'TestRecipe'} imageSource={require('../res/bread2.png')} title="English White Bread"/>
+                                        </View>
+                                    </ScrollView>
+                                </>
+                            }
+                        </>
+                    }
                 </View>
             </View>
     );
@@ -203,16 +191,19 @@ const styles = StyleSheet.create({
 
     rowButtonContainer: {
         width: '24%',
-        padding: 9,
-        marginVertical: 5,
+        padding: 5,
+        marginVertical: 8,
         alignItems: 'center',
         alignSelf: 'center',
-        backgroundColor: '#3B71F3'
+        backgroundColor: 'white',
+        borderRadius: 50,
+        borderColor: '#708090',
+        borderWidth: 1,
+
     },
 
     customButtonText: {
-        fontWeight: 'bold',
-        color: 'white',
+        color: 'black',
     },
 
     inputContainer: {
